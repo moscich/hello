882888feadac52e09c7f853af6d9997fb7c8ba3f
Marek Mościchowski <marek.moscichowski@miquido.com>
2016-12-17 20:38:47 +0100

    database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+// var conString = process.env.DATABASE_URL
+var conString = "postgres://hello:test_password@localhost:5432/hello"
+console.log("con = " + conString)
+var client = new pg.Client(conString);
+client.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var query = client.query("SELECT * FROM users");
+  query.on('row', function(row) {
+  	res.render('index', { title: row.name});
+  })
 });
 
 module.exports = router;
